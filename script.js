7094b1d04d2dca7f5797f5730be4d8e91108323c
Russell <russelldev@protonmail.com>
2021-02-21 12:47:00 +0000

    Add item remover funcs

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 const contents = document.querySelectorAll('.content');
 const listItem = document.querySelectorAll('nav ul li');
+
+listItem.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        hideAllContents();
+        hideAllItems();
+    });
+});
+
+function hideAllContents() {
+    contents.forEach(content => content.classList.remove('show'));
+}
+
+function hideAllItems() {
+    listItem.forEach(item => item.classList.remove('show'));
+}
